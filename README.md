f15c76a5709a3a59b0549a75959cc6756048178b
Jon_shen <jerosshen@gmail.com>
2016-11-28 17:14:39 +0800

    use init

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# mingpian
+# express init
+
+### About
+
+This program creates by express + jade + mongodb
+
+### History
+
+* 2016-11-28  Finish v0.0.1
+
+
+### License
+MIT
--- app.js
+++ app.js
@@ -1,81 +1,85 @@
-'use strict';
+var express = require('express');
+var app = express();
+var path = require('path');
+var favicon = require('serve-favicon');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var mongoose = require('mongoose');
+var ueditor = require('ueditor');
+const compression = require('compression');      // 压缩
+var config = require('./config')
 
-require('source-map-support').install();
-require("babel-polyfill");
-const Koa = require('koa');
-const views = require('koa-views');
-const Router = require('koa-router');
-const log4js = require('log4js');
-var fs = require('fs')
-var path = require('path')
-var koaStaticPlus = require('koa-static-plus')
+
+// 开发用的模块
+var logger = require('morgan');                  // HTTP request logger middleware for node.js
+
+var multipart = require('connect-multiparty');
 
 
-const convert = require('koa-convert');
-// import Koa from 'koa';
-// import views from 'koa-views';
-// import router from 'koa-router';
+//for the offline storage
+var session = require('express-session')
+var MongoStore = require('connect-mongo')(session)     // 用于本地 session
+var cookieParser = require('cookie-parser')
+var dburl = 'mongodb://localhost/' + config.name;
 
-// koa需要用new
-const app = new Koa();
+// database
+mongoose.connect(dburl)
+// mongoose.on('error', console.error.bind(console,'连接错误:'));    // 报错，not a function
 
-const router = new Router();
+// program config
+app.set('port', process.env.PORT || config.port);                  // 设置端口号
+app.set('views', './views/pages');
+app.set('view engine', 'pug');
 
-const LOG = log4js.getLogger('file')
+app.use(express.static('public'));
 
-app.use(koaStaticPlus(path.join(__dirname, '/dist'), {
-    pathPrefix: ''
+app.use(compression());
+
+
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+
+
+// 如果有使用 ueditor, 这个上传和 ueditor的上传模块冲突，所以要放在 ueditor后面
+app.use(multipart());
+
+// 用户登录持久化
+app.use(cookieParser());        //session 依赖的中间件  存储sessionid
+app.use(session({               //用来本地存储信息 store 对象
+  secret: config.secret,
+  resave: false,                // 不设置为 true, 登录会失败
+  saveUninitialized: true,
+  cookie: {
+    secure: false,
+    maxAge: 43200000              // 半个小时
+  },                            // 这次是这里设置为false才登陆成功，resave 设置false， 奇怪
+  store: new MongoStore({
+    url: dburl,
+    collection: 'sessions'      // 这条不懂，为什么是sessions是
+  })
 }))
 
-// 模板引擎设置
-app.use(views(path.join(__dirname + '/views'), {
-  map: {
-    html: 'pug'
-  },
-  extension: 'pug'
-}));
-// app.use(views(__dirname + '/views', {
-//   extension: 'jade'
-// }));
+// 本地变量设置
+app.locals.moment = require('moment')
+// app.locals._env = app.get('env')                   //pod里不能用这个
+app.locals._env = process.env.NODE_ENV                // 判定是否开发环境，是就调用本地资源
 
 
-// app.use(router.get('/', function *() {
-// 	yield this.render(clawler);
-//   // await ctx.render('clawler.pug')
-// }));
+// 开发使用套件
+if ('development' === app.get('env')){              // 如果是开发环境
+  app.set('showStackErr', true)                     // 打印错误信息
+  app.use(logger(':method:url:status'))             // 请求相关信息
+  app.locals.pretty = true                          // 不压缩源码
+  mongoose.set('debug', true)                       // 数据库请求信息
+}
 
-// router.get('/k', function *() {
-// 	yield this.render('clawler.pug');
-//   // await ctx.render('clawler.pug')
-// });
-
-router.get('/k', async function (ctx, next) {
-  // ctx.state = {
-  //   title: 'koa2 title'
-  // };
-
-  await ctx.render('clawler.pug', {
-  });
-})
+// 路由
+require('./routes/router')(app);
 
 
-app.use(function (ctx, next) {
-    console.log(ctx.method, ctx.url)
-    // Router.match(ctx, next)
-})
-// const port = app.get('port') || 3005;
-
-
-
-// 如果错误发生在 请求/响应 环节，并且其不能够响应客户端时，Contenxt 实例也会被传递到 error 事件监听器的回调函数里。
-app.on('error', function(err, ctx){
-  LOG.error('server error', err, ctx);
+// 最后启动程序
+var server = app.listen(app.get('port'), function() {
+  console.log('网站程序已启动，端口： ' + server.address().port);
 });
-
-// http.createServer(app.callback()).listen(3000);
-// 这意味着您可以同时支持 HTTPS 和 HTTPS，或者在多个端口监听同一个应用。
-app.listen(5000, function(){
-  console.log('应用已经启动，端口5000');
-});
-
-module.exports = app
